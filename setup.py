885884633e27a63e5f2f805097c9005595fc7848
Cong Liang <cong.liang@outlook.com>
2020-05-10 22:37:38 -0500

    add posterior estimator

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pylce",
+    version="0.0.1",
+    author="Cong Liang",
+    author_email="author@example.com",
+    description="level of correlated evolution estimation",
+    packages=find_packages()
+)
