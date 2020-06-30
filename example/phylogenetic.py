fix pic calculation

example/phylogenetic.py
@@ -2,14 +2,23 @@
 import dendropy as dp
 import numpy as np
 import pandas as pd
+import yaml
+import matplotlib.pyplot as plt
 
+from munch import Munch
 from typing import Dict, Callable
 from pylce.pic import PIC
 # -
 
-tree = dp.Tree.get(path = '/Users/cong/Documents/Projects/2020-per-gene-LCE/data/brawand_tree.nwk', schema = 'newick')
+# tree = dp.Tree.get(path = '/Users/cong/Documents/Projects/2020-per-gene-LCE/data/brawand_tree.nwk', schema = 'newick')
+# tree = dp.Tree.get(path = '/Users/cong/Documents/Projects/2020-per-gene-LCE/data/podos_tree.nwk', schema = 'newick')
+tree = dp.Tree.get_from_path('/Users/cong/Documents/Projects/2020-per-gene-LCE/data/acer_species_modified_branch.nwk', schema = 'newick') # acer, L = 160, n = 55
+# tree = dp.Tree.get_from_path('/Users/cong/Documents/Projects/2020-per-gene-LCE/data/pinus_species.nwk', schema = 'newick') # pinus, L = 73, n = 111
 
 # +
+# tree.print_plot()
+# -
+
 idx = 1
 for nd in tree.postorder_node_iter():
     if(nd.taxon == None): ## Add internal label name
@@ -24,39 +33,69 @@ for nd in tree.postorder_node_iter():
     except:
         print('+', nd.label,  nd.edge_length) # check iter function
     # break
-    
-# check internal node iter
-for nd in tree.postorder_internal_node_iter():
-    print(nd.label, ', distance to parent: ', nd.edge_length)
-    [a, b] = nd.child_nodes() 
-    print(nd.num_child_nodes())
-    print('Children:', a.label, ', ', b.label)
-# -
 
-tree.print_plot()
+test = {}
+for species in tree.taxon_namespace:
+    test[species.label] = 0
+
+## vary the scale of values
+L = 160
+ts_lambda = np.array( [10/L, 3.3/L, 1/L, 0.33/L, 0.1/L] )
+t_scale = ts_lambda * L
+ts_lambda
+
+# %matplotlib notebook
+
+species = [sp.label for sp in tree.taxon_namespace]
+nspecies = len(species)
+xx, yy = np.meshgrid(np.arange(nspecies-1), np.arange(nspecies))
 
 # +
-# [c for c in dir(nd) if not c.startswith('_')] # check node content
+attr = {'ts_lambda': np.nan}
+res = {}
+fig = plt.figure(figsize=(15, 6))
+cmap = 'coolwarm'
+alpha = 0.5
+size_contrast = .1
+size_node = 50
+
+for i, ts_l in enumerate(ts_lambda):
+    ax_contrast = fig.add_subplot(2, len(ts_lambda)+1, i + 1)
+    ax_node = fig.add_subplot(2, len(ts_lambda)+1, i+2+len(ts_lambda))
+    idx = 'LT=' + str(ts_l * L)
+    
+    attr['ts_lambda'] = ts_l
+    picOU = PIC(tree, test, 'OU', attr)
+    picOU.calc_contrast()
+        
+    # plot coef for contrast and node value
+    zz_ou = np.abs(picOU.contrast_coef)*size_contrast /np.abs(picOU.contrast_coef.iloc[nspecies-1,nspecies-2])
+    ou_color = picOU.contrast_coef.apply(lambda x: [0 if y <= 0 else 1 for y in x])
+    ax_contrast.scatter(xx, yy, zz_ou, c = ou_color.values, cmap=cmap, alpha = alpha)
+    ax_contrast.set_title(idx + ' contrast coef')
+    
+    zz_ou = np.abs(picOU.node_coef)*size_node/0.5
+    ou_color = picOU.node_coef.apply(lambda x: [0 if y <= 0 else 1 for y in x])
+    ax_node.scatter(xx, yy, zz_ou, c = ou_color.values, cmap = cmap, alpha = alpha)
+    ax_node.set_title(idx + ' node coef')
+    
+attr = {}
+picBM = PIC(tree, test, 'BM', attr)
+picBM.calc_contrast()
+
+ax_contrast = fig.add_subplot(2, len(ts_lambda)+1, len(ts_lambda) + 1)
+zz_bm = np.abs(picBM.contrast_coef)*size_contrast / np.abs(picBM.contrast_coef.iloc[nspecies-1,nspecies-2])
+bm_color = picBM.contrast_coef.apply(lambda x: [0 if y <= 0 else 1 for y in x])
+ax_contrast.scatter(xx, yy, zz_bm, c = bm_color.values, cmap=cmap, alpha = alpha)
+ax_contrast.set_title('BM contrast coef')
+ax_node = fig.add_subplot(2, len(ts_lambda)+1, 2*len(ts_lambda)+2)
+
+zz_bm = np.abs(picBM.node_coef)*size_node/0.5
+bm_color = picBM.node_coef.apply(lambda x: [0 if y <= 0 else 1 for y in x])
+ax_node.scatter(xx, yy, zz_bm, c = bm_color.values, cmap = cmap, alpha = alpha)
+ax_node.set_title('BM node coef')
+
+plt.tight_layout()
+plt.show()
 # -
-
-tree.gk_lambda = .2
-
-test = {'Gallus gallus -chicken': 1,
-'Ornithorhynchus anatinus -platypus': 2,
-'Monodelphis domestica -opossum': 3,
-'Mus musculus -mouse': 4,
-'Pongo abelii -orangutan': 5,
-'Gorilla gorilla -gorilla': 6,
-'Homo sapiens -human': 7,
-'Pan paniscus -bonobo': 8,
-'Pan troglodytes -chimpanzee': 9,
-'Macaca mulatta -macaque': 10}
-test
-
-pic = PIC(tree, test)
-res = pic.calc_contrast()
-np.exp(-10)
-
-pd.DataFrame(res).T.sort_index()
-
 

pylce/pic.py
@@ -2,53 +2,95 @@ from munch import Munch
 from typing import Dict, Callable
 import dendropy as dp
 import numpy as np
+import pandas as pd
 
 # from .tree import Node, Tree
-from .util import *
+from .pic_ou_util import OUcalculator 
+from .pic_bm_util import BMcalculator
 
 
 class PIC:
-    def __init__(self, tree: dp.Tree, vec: Dict[str, float]):
-        self._tree = tree
-        self._vec = vec
+    def __init__(self, tree: dp.Tree, taxa_val: Dict[str, float], mode: str, attr: Dict[str, float]):
+        self.tree = tree
+        self.taxa_val = taxa_val
+        self.mode = mode
+        if mode == 'OU':
+            calc_class = OUcalculator
+        elif mode =='BM':
+            calc_class = BMcalculator
+        else:
+            raise ValueError('Unkonwn mode %r.' % mode )
+        self._calculator = calc_class(attr)
 
-    def calc_contrast(self):
-        results = {}
-        for nd in self._tree.postorder_node_iter():
+        self.contrasts = {}
+        self.contrast_coef = None
+        self.node_coef = None
+
+    def calc_contrast(self): 
+        '''
+        Warning: 1) internal node name will be overwritten. 
+        2) only bifurcation trees allowed.
+        '''
+        # Initialize
+        contrasts = {}
+        species = [nd.label for nd in self.tree.taxon_namespace]
+        contrast_coef = pd.DataFrame( np.identity(len(species)) )
+        contrast_coef.index = species
+        contrast_coef.columns = species
+        node_coef = contrast_coef.copy()
+
+        # Add internal node name
+        idx = 1
+        for nd in self.tree.postorder_node_iter():
+            if(nd.taxon == None): ## Add internal label name
+                nd.label = "Internal_" + str(idx)
+                idx = idx + 1
+            else:
+                nd.label = nd.taxon.label
+
+        # postorder traversal nodes
+        for nd in self.tree.postorder_node_iter():
             edge_length = nd.edge_length if nd.edge_length else 0
 
             # leaf
-            hash_id = nd.label
             if nd.num_child_nodes() == 0:
                 pars = {
                     'is_contrast': False,
-                    'contrast_value': 0.,
-                    'contrast_variance': 0.,
-                    'normalized_contrast_value': 0.,
+                    'contrast_standardized': 0.,
                     'dist_to_parent': edge_length, # modified distance to parent
-                    'nd_value': self._vec[nd.label]
+                    'nd_value': self.taxa_val[nd.label]
                 }
-                results[hash_id] = Munch(pars)
-            # node with two child
-            # contrast node
+                contrasts[nd.label] = Munch(pars)
+            # internal node
             else:
                 # child_nodes() returns a list
-                [left_child, right_child] = nd.child_nodes()
-                left_res = results[left_child.label]
-                right_res = results[right_child.label]
+                # only bifurcation trees allowed
+                left_child, right_child = nd.child_nodes()
+                left_res = contrasts[left_child.label]
+                right_res = contrasts[right_child.label]
 
-                contrast_value = calc_contrast_value(left_res, right_res, self._tree.gk_lambda)
-                contrast_variance = calc_contrast_variance(left_res, right_res, self._tree.gk_lambda)
-                nd_value = calc_nd_value(left_res, right_res, self._tree.gk_lambda)
-                nd_addition_dist_to_parent = calc_addition_dist_to_parent(left_res, right_res, self._tree.gk_lambda)
+                contrast_val = self._calculator.calc_contrast_standardized(left_res, right_res)
+                nd_val = self._calculator.calc_nd_value(left_res, right_res)
+                nd_addition_dist_to_parent = self._calculator.calc_addition_dist_to_parent(left_res, right_res)
 
                 pars = {
                     'is_contrast': True,
-                    'contrast_value': contrast_value,
-                    'contrast_variance': contrast_variance,
-                    'normalized_contrast_value': contrast_value / np.sqrt(contrast_variance),
+                    'contrast_standardized': contrast_val.contrast_standardized,
                     'dist_to_parent': edge_length + nd_addition_dist_to_parent,
-                    'nd_value': nd_value
+                    'nd_value': nd_val.node_value
                 }
-                results[hash_id] = Munch(pars)
-        return results
+                contrasts[nd.label] = Munch(pars)
+
+                # add contrast and node value calculation parameters
+                contrast_coef[nd.label] = contrast_val.left_par * node_coef[left_child.label] + contrast_val.right_par * node_coef[right_child.label]
+                node_coef[nd.label] = nd_val.left_par * node_coef[left_child.label] + nd_val.right_par * node_coef[right_child.label]
+        
+        leaf_order = []
+        for nd in self.tree.postorder_internal_node_iter():
+            for child in nd.child_nodes():
+                if child.is_leaf():
+                    leaf_order.append(child.label)
+
+        self.contrasts = pd.DataFrame(contrasts).filter(like = 'Internal_') #.drop(['is_contrast', 'dist_to_parent'])
+        self.contrast_coef = contrast_coef.loc[leaf_order].filter(like = 'Internal_')
+        self.node_coef = node_coef.loc[leaf_order].filter(like = 'Internal_')