a080b9b29373b52de37b639f290fb04c9cba2919
Donghai Pan <donghai.pan@outlook.com>
2020-09-23 17:02:14 -0500

    fix path in example_pic

--- example/example_pic.py
+++ example/example_pic.py
@@ -1,28 +1,35 @@
-import sys
-sys.path.append('../')
+from pathlib import Path
 
 import dendropy as dp
-import numpy as np
 
-from oupic.pic import PIC
 from oupic.ou_calculator import OUCalculator
-from oupic.bm_calculator import BMCalculator
+from oupic.pic import PIC
 
+# resolve file path
+DIR_NAME = Path(__file__).resolve().parents[1]
+filename = DIR_NAME / "data" / "brawand_tree.nwk"
 
-tree = dp.Tree.get_from_path('../data/brawand_tree.nwk', schema = 'newick')
+# load tree
+tree = dp.Tree.get_from_path(filename, schema="newick")
 
+# generate sample data
 taxa_val = {}
 i = 1
 for taxon in tree.taxon_namespace:
     taxa_val[taxon.label] = i
     i += 1
 
+# initialize ou calculator with lambda=0.5
 calculator = OUCalculator(ts_lambda=0.5)
+
+# initialize pic
 pic = PIC(tree, calculator, taxa_val)
+
 pic.calc_contrast()
 
 
-format_str = "=" * 20 + " %s " + "=" * 20 
+# print result
+format_str = "=" * 30 + " %s " + "=" * 30
 
 print(format_str % "node coef")
 print(pic.node_coef)
